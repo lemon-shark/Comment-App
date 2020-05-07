595493e521b7ff65bb7c316294ad00683a9eeb29
lemon-shark <shiqiaozhu@hotmail.com>
2020-05-06 20:30:45 -0400

    Initial commit

---
+++ src/CommentInput.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import CommentInput from './CommentInput';
+import CommentList from './CommentList';
+
+class CommentApp extends Component {
+    render(){
+        return(
+            <div>CommentInput</div>
+        )
+    }
+}
+
+export default CommentApp;
