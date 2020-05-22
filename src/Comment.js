0cb7376f4f5efec044aec8ba4e88823c65074d82
lemon-shark <shiqiaozhu@hotmail.com>
2020-05-22 15:33:02 -0400

    Revert "Save comments to local storage & show comment's time"
    This reverts commit a50d19ce176a7694e74670de93e4a6a685d689b8.

--- src/Comment.js
+++ src/Comment.js
@@ -1,67 +1,13 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types'
 
 class Comment extends Component {
-    static propTypes={
-        comment:PropTypes.object.isRequired,
-        onDeleteComment:PropTypes.func,
-        index:PropTypes.number
-    }
-
-    constructor(){
-        super()
-        this.state={timeString:''}
-    }
-    componentWillMount() {
-        this._updateTimeString();
-        //update the timeString every 5 seconds
-        this._timer=setInterval(
-            this._updateTimeString.bind(this),
-            5000
-        )
-    }
-
-    _updateTimeString(){
-        const comment = this.props.comment;
-        const duration = (+Date.now()-comment.createdTime)/1000
-        this.setState({
-            timeString: duration>60
-              ?`${Math.round(duration/60)} minute(s) ago`
-                :`${Math.round(Math.max(duration,1))} second(s) ago`
-        })
-    }
-
-    handleDeleteComment(){
-        if(this.props.onDeleteComment){
-            this.props.onDeleteComment(this.props.index)
-        }
-    }
-
-    _getProcessedContent(content){
-        return content
-            .replace(/&/g,"&amp;")
-            .replace(/</g,"&lt;")
-            .replace(/>/g,"&gt;")
-            .replace(/"/g,"&quot;")
-            .replace(/'/g,"&#039;")
-            .replace(/`([\S\s]+?)`/g,`<code>$1</code>`)
-    }
-
     render(){
-        const {comment}=this.props
         return(
             <div className='comment'>
-                <div className='comment-username'>
-                    <span>{comment.username}</span>
+                <div className='comment-user'>
+                    <span>{this.props.comment.username}</span>
                 </div>
-                <p dangerouslySetInnerHTML={{
-                    __html:this._getProcessedContent(comment.content)
-                }} />
-                <span className='comment-createdtime'>
-                    {this.state.timeString}
-                </span>
-                <span onClick={this.handleDeleteComment.bind(this)}
-                    className='comment-delete'>Delete</span>
+                <p>{this.props.comment.content}</p>
             </div>
         )
     }
--- src/CommentInput.js
+++ src/CommentInput.js
@@ -1,41 +1,14 @@
 import React, {Component} from 'react';
 import CommentInput from './CommentInput';
 import CommentList from './CommentList';
-import PropTypes from 'prop-types'
 
 class CommentApp extends Component {
-    static propTypes = {
-        onSubmit:PropTypes.func
-    }
     constructor(){
         super()
         this.state={
             username:'',
             content:''
         }
-    }
-
-    componentWillMount() {
-        this._loadUsername()
-    }
-
-    _loadUsername(){
-        const username = localStorage.getItem('username')
-        if(username){
-            this.setState({username})
-        }
-    }
-
-    componentDidMount() {
-        this.textarea.focus()
-    }
-
-    _saveUsername(username){
-       localStorage.setItem('username',username)
-    }
-
-    handleUsernameBlur(event){
-        this._saveUsername(event.target.value)
     }
 
     handleUsernameChange(event){
@@ -53,11 +26,8 @@ class CommentApp extends Component {
     handleSubmit(){
         //if pass in onSubmit
         if(this.props.onSubmit){
-            this.props.onSubmit({
-                username: this.state.username,
-                content:this.state.content,
-                createdTime:+new Date()
-            });
+            const{username, content} = this.state;
+            this.props.onSubmit({username, content});
         }
         //use setStates to clear user input, but keep the user name to ensure
         //user experience
@@ -72,7 +42,6 @@ class CommentApp extends Component {
                     <span className='comment-field-name'>Username</span>
                     <div className='comment-field-input'>
                         <input value={this.state.username}
-                               onBlur={this.handleUsernameBlur.bind(this)}
                                onChange={this.handleUsernameChange.bind(this)}/>
 
                     </div>
@@ -80,8 +49,7 @@ class CommentApp extends Component {
                 <div className='comment-field'>
                     <span className='comment-field-name'>Message</span>
                     <div className='comment-field-input'>
-                        <textarea ref={(textarea)=> this.textarea=textarea}
-                                  value={this.state.content}
+                        <textarea value={this.state.content}
                                   onChange={this.handleContentChange.bind(this)}/>
                     </div>
                 </div>
